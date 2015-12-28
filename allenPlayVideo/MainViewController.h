199f6b510ad7ec8e22f45cad8be24f0734f2fbab
allen_hsu <allen_hsu@administratordeMacBook-Air-12.local>
2015-12-28 10:55:59 +0800

    刪除多餘的程式碼。

--- allenPlayVideo/MainViewController.h
+++ allenPlayVideo/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  allenPlayVideo
 //
 //  Created by allen_hsu on 2015/12/28.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UIViewController
 
 @end
-
