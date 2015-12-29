301937af4372308254e9af079f8afdbdf376224c
allen_hsu <allen_hsu@administratordeMacBook-Air-12.local>
2015-12-29 15:44:15 +0800

    fix 。

--- PlayVideoView.h
+++ PlayVideoView.h
@@ -11,21 +11,6 @@
 
 @interface PlayVideoView : UIView
 
-@property (nonatomic, weak) IBOutlet UIView *topBarView;
-@property (nonatomic, weak) IBOutlet UIView *controlButtonView;
-@property (nonatomic, weak) IBOutlet UIButton *playVideoButton;
-@property (nonatomic, weak) IBOutlet UIButton *fullScreenButton;
-@property (nonatomic, weak) IBOutlet UILabel *currentlyTimeLabel;
-@property (nonatomic, weak) IBOutlet UILabel *totalTimeLabel;
-@property (nonatomic, weak) IBOutlet UISlider *videoSlider;
-@property (nonatomic, strong) AVPlayer *player;
-@property (nonatomic, strong) AVPlayerLayer *playerLayer;
-
-@property (assign, nonatomic) BOOL isPlayingVideos;
-@property (assign, nonatomic) BOOL isSliderMoving;
-@property (assign, nonatomic) int playIndex;
-@property (strong, nonatomic) NSArray *dataSoruce;
-
 - (void)checkVideoData:(NSArray *)videoData;
 - (UIImage *)videoImage:(int)keyValue;
 
--- PlayVideoView.m
+++ PlayVideoView.m
@@ -7,9 +7,23 @@
 //
 
 #import "PlayVideoView.h"
-@interface PlayVideoView()
+@interface PlayVideoView ()
 
 @property (weak, nonatomic) IBOutlet UIView *playVideoView;
+@property (weak, nonatomic) IBOutlet UIButton *playAndPauseButton;
+@property (weak, nonatomic) IBOutlet UIButton *rateButton;
+@property (weak, nonatomic) IBOutlet UISlider *videoSlider;
+@property (weak, nonatomic) IBOutlet UILabel *currentTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalTimeLabel;
+
+@property (strong, nonatomic) NSArray *dataSoruce;
+@property (strong, nonatomic) AVPlayer *player;
+@property (strong, nonatomic) AVPlayerLayer *playerLayer;
+
+@property (assign, nonatomic) BOOL isChangeRate;
+@property (assign, nonatomic) BOOL isPlayingVideos;
+@property (assign, nonatomic) BOOL isSliderMoving;
+@property (assign, nonatomic) int playIndex;
 
 @end
 
@@ -18,24 +32,43 @@
 #pragma mark - IBAction Action
 
 - (IBAction)periousVideoButtonAction:(id)sender {
+    // 上一首
     [self periousVideoButtonAction];
 }
 
 - (IBAction)nextVideoButtonAction:(id)sender {
+    // 下一首
     [self nextVideoButtonAction];
 }
 
 - (IBAction)playMusicButtonAction:(id)sender {
     // 播放功能
+    if (self.isPlayingVideos) {
+        [self.playAndPauseButton setTitle:@"Pause" forState:UIControlStateNormal];
+        [self playVideo];
+    }
+    else {
+        [self.playAndPauseButton setTitle:@"Play" forState:UIControlStateNormal];
+        [self pauseVideo];
+    }
     self.isPlayingVideos = !self.isPlayingVideos;
 }
 
 - (IBAction)fullScreenButtonAction:(id)sender {
-
+    // 全畫面
 }
 
 - (IBAction)rateButtonAction:(id)sender {
-
+    // 播放速率
+    if (self.isChangeRate) {
+        self.player.rate = 1.0;
+        [self.rateButton setTitle:@"x2" forState:UIControlStateNormal];
+    }
+    else {
+        self.player.rate = 2.0;
+        [self.rateButton setTitle:@"normal" forState:UIControlStateNormal];
+    }
+    self.isChangeRate = !self.isChangeRate;
 }
 
 #pragma mark * Slider
@@ -45,10 +78,14 @@
 }
 
 - (IBAction)videoSliderUpInside:(id)sender {
-    self.isSliderMoving = NO;
-    CMTime newTime = CMTimeMakeWithSeconds(self.videoSlider.value, self.videoSlider.maximumValue);
-    [self.player seekToTime:newTime];
+    CMTime dragedCMTime = CMTimeMakeWithSeconds(self.videoSlider.value, self.videoSlider.maximumValue);
+    [self.player seekToTime:dragedCMTime completionHandler: ^(BOOL finished) {
+         [self.player play];
+     }];
+    self.isSliderMoving = !self.isSliderMoving;
 }
+
+#pragma mark - private instance method
 
 #pragma mark * init
 
@@ -56,17 +93,13 @@
     self = [super initWithFrame:frame];
     if (self) {
         self = [[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class]) owner:self options:nil][0];
-        self.isPlayingVideos = YES;
     }
     return self;
 }
 
-#pragma mark - private instance method
-
-#pragma mark * init
-
 - (void)checkVideoData:(NSArray *)videoData {
     if (videoData.count) {
+        self.isChangeRate = NO;
         self.dataSoruce = videoData;
         self.playIndex = 0;
         [self playVideoConfigure:self.playIndex];
@@ -90,16 +123,36 @@
     [self.playVideoView.layer.sublayers makeObjectsPerformSelector:@selector(removeFromSuperlayer)];
     [self.playVideoView.layer addSublayer:self.playerLayer];
     [self.player play];
+    self.isPlayingVideos = YES;
     [self addObservers];
 }
 
 #pragma mark * misc
 
-- (NSString *)formatTime:(int)num {
-    // 秒轉分鐘
-    int sec = num % 60;
-    int min = num / 60;
-    return [NSString stringWithFormat:@"%02d:%02d", min, sec];
+- (void)addProgressBarUpdate {
+    int totalSeconds = CMTimeGetSeconds(self.player.currentItem.asset.duration);
+    self.videoSlider.maximumValue = totalSeconds;
+    // 给播放器增加進度更新
+    __weak typeof(self) weakSelf = self;
+    [self.player addPeriodicTimeObserverForInterval:CMTimeMake(1, 2) queue:dispatch_get_main_queue() usingBlock: ^(CMTime time) {
+         if (!weakSelf.isSliderMoving) {
+             CGFloat currentTime = CMTimeGetSeconds(weakSelf.player.currentTime);
+             weakSelf.videoSlider.value = currentTime;
+             weakSelf.currentTimeLabel.text = [weakSelf convertTime:currentTime];
+         }
+     }];
+}
+
+- (NSString *)convertTime:(CGFloat)second {
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:second];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    if (second / 3600 >= 1) {
+        [formatter setDateFormat:@"HH:mm:ss"];
+    }
+    else {
+        [formatter setDateFormat:@"mm:ss"];
+    }
+    return [formatter stringFromDate:date];
 }
 
 - (UIImage *)videoImage:(int)keyValue {
@@ -117,46 +170,32 @@
 #pragma mark - KVO
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-    if (self.player == object && [keyPath isEqualToString:@"status"]) {
-        if (self.player.status == AVPlayerStatusReadyToPlay) {
-            //顯示總時間
-            int totalSeconds = CMTimeGetSeconds(self.player.currentItem.asset.duration);
-            self.totalTimeLabel.text = [self formatTime:totalSeconds];
-            //設定 currentVideoSlider 的長度
-            self.videoSlider.maximumValue = totalSeconds;
-            // 解除 retain cycle
-            __weak typeof(self) weakSelf = self;
-            // 给播放器增加進度更新
-            [self.player addPeriodicTimeObserverForInterval:CMTimeMake(1, 2) queue:dispatch_get_main_queue() usingBlock: ^(CMTime time) {
-                 if (!weakSelf.isSliderMoving) {
-                     int currentTime = CMTimeGetSeconds(weakSelf.player.currentTime);
-                     weakSelf.videoSlider.value = currentTime;
-                     weakSelf.currentlyTimeLabel.text = [weakSelf formatTime:currentTime];
-                 }
-             }];
-        }
-        else if (self.player.status == AVPlayerStatusFailed) {
-            NSLog(@"檔案錯誤");
-        }
-        else if (self.player.status == AVPlayerStatusUnknown) {
-            NSLog(@"沒有任何檔案數據");
+    AVPlayerItem *playerItem = (AVPlayerItem *)object;
+    if ([keyPath isEqualToString:@"status"]) {
+        AVPlayerStatus status = [change[@"new"] intValue];
+        if (status == AVPlayerStatusReadyToPlay) {
+            CGFloat totalSeconds = CMTimeGetSeconds(playerItem.duration);
+            self.totalTimeLabel.text = [self convertTime:totalSeconds];
+            [self addProgressBarUpdate];
+            NSLog(@"addProgressBarUpdate");
         }
     }
+
 }
 
 - (void)addObservers {
     // 使用 KVO 監聽 playerItem 狀態
-    [self.player addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+    [self.player.currentItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
     // 使用 NSNotificationCenter 監聽 playerItem：如果播放完就直接下一首
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(nextVideoButtonAction)
                                                  name:AVPlayerItemDidPlayToEndTimeNotification
-                                               object:self.player.currentItem];
+                                               object:nil];
 }
 
 - (void)removeAllObserver {
-    [self.player removeObserver:self forKeyPath:@"status"];
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:AVPlayerItemDidPlayToEndTimeNotification object:self.player.currentItem];
+    [self.player.currentItem removeObserver:self forKeyPath:@"status"];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
 }
 
 
@@ -172,6 +211,14 @@
     self.playIndex--;
     self.playIndex %= self.dataSoruce.count;
     [self playVideoConfigure:self.playIndex];
+}
+
+- (void)playVideo {
+    [self.player play];
+}
+
+- (void)pauseVideo {
+    [self.player pause];
 }
 
 @end
