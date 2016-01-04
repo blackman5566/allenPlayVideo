81149a545c6b1584c2cb0258b2f30495f9adaf78
Allen_shiu <pt7922310@gmail.com>
2016-01-04 12:29:46 +0800

    add model 。

---
+++ allenPlayVideo/DownloadModel.h
@@ -0,0 +1,13 @@
+//
+//  DownloadModel.h
+//  allenPlayVideo
+//
+//  Created by 許佳豪 on 2016/1/4.
+//  Copyright © 2016年 allen_hsu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloadModel : NSObject
+
+@end
---
+++ allenPlayVideo/DownloadModel.m
@@ -0,0 +1,13 @@
+//
+//  DownloadModel.m
+//  allenPlayVideo
+//
+//  Created by 許佳豪 on 2016/1/4.
+//  Copyright © 2016年 allen_hsu. All rights reserved.
+//
+
+#import "DownloadModel.h"
+
+@implementation DownloadModel
+
+@end
