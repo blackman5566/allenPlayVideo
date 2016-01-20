793f4bb6f8b0171fcbfa028d6cc0f04264be9473
Allen_shiu <pt7922310@gmail.com>
2016-01-20 15:20:34 +0800

    新增註解。

--- allenPlayVideo/Commom/Views/PlayVideoView/PlayVideoView.h
+++ allenPlayVideo/Commom/Views/PlayVideoView/PlayVideoView.h
@@ -12,9 +12,24 @@
 typedef void (^RemoveVideoBackBlock)();
 
 @interface PlayVideoView : UIView
-
+/*
+ @abstract 初始化 Video 參數設定。
+ */
 - (void)initVideoData:(NSMutableArray *)videoData;
+
+/*
+ @abstract 回傳 Video 圖片。
+ */
 - (UIImage *)videoImage:(NSInteger)videoindex;
+
+/*
+ @abstract 選擇 Video 檔案並播放。
+ */
 - (void)didVideoSelect:(NSUInteger)index;
+
+/*
+ @abstract 刪除 Video 檔案。
+ */
 - (void)removeVideo:(NSInteger )index callBack:(RemoveVideoBackBlock)completion;
+
 @end
